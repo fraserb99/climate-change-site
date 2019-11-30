8aebfde66d305cf10098bb63f0508f794aac3870
Blair Mckenzie <wdb17134@uni.strath.ac.uk>
2019-11-30 19:45:44 +0000

    Futher work on homepage and styling

--- front-end/src/slices/Home/HomePage.js
+++ front-end/src/slices/Home/HomePage.js
@@ -5,34 +5,62 @@ export const HomePage = () => (
     <Row>
     <Col lg={3} className='details-left'>
     <div className='page-header'>
-    <h1>Home Page</h1>
 </div>
 </Col>
 
-    <Col lg={{span: 7, offset: 0}} className='home-content'>
+    <Col lg={{span: 12, offset: 0}} className='home-content'>
     <div className='home-body'>
-    <h2>What is Climate Change?</h2>
-    <p max-width="1em">
+    <h1 className='tree-title'>Tree Hugging is my life (Need to change this)</h1>
+    <h2 className='subsections'>Who are we?</h2>
+    <p className='paragraphs'>
+    (Insert decent name here) are climate change activists that believe in changing the world by planting more trees to reduce CO2 emissions into the atmosphere.
+    We take pride in the effort we make in making a global impact on climate change and you can start helping today. Any discussion you would like to have head
+    on over to our forums.
+    </p>
+    <h2 className='subsections'>What is Climate Change?</h2>
+    <p className='paragraphs'>
         From shifting weather patterns that threaten food production, to rising sea levels that increase the risk of catastrophic flooding, the impacts of climate change are global in scope and unprecedented in scale.
         Without drastic action today, adapting to these impacts in the future will be more difficult and costly.
     </p>
-    <h2>Causes of Climate Change</h2>
+    <h2 className='subsections'>Causes of Climate Change</h2>
+    <p className='paragraphs'>
 
-    <h2>Effects of Climate Change</h2>
+    </p>
+    <h2 className='subsections'>Effects of Climate Change</h2>
+    <p className='paragraphs'>
+    Climate change is cripling the world that we live in, if nothing is done to combat the rapid changes to our planet
+    this will have serious implications to our daily lifes.
+    </p>
+    <h2 className='subsections'>Possible solutions</h2>
+    <p className='paragraphs'>
 
-    <h2>Possible solutions</h2>
-    <h2>What you can do to help</h2>
-    <h4>Useful Resources</h4>
-    <iframe width="560" height="315"
+    </p>
+    <h2 className='subsections'>What you can do to help</h2>
+    <p className='paragraphs'>
+
+    </p>
+    <h2 className='subsections'>Videos on climate change</h2>
+<p className='paragraphs'>
+
+
+    <iframe width="420" height="245"
     src="https://www.youtube.com/embed/G4H1N_yXBiA" frameborder="0"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
     </iframe>
-    <iframe width="560" height="315"
+    <iframe width="420" height="245"
     src="https://www.youtube.com/embed/yvDRQe2oCt4" frameborder="0"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
     </iframe>
+    <iframe width="420" height="245"
+    src="https://www.youtube.com/embed/vpTHi7O66pI" frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    </iframe>
+    <iframe width="420" height="245"
+    src="https://www.youtube.com/embed/oJAbATJCugs" frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    </iframe>
+</p>
     <footer>
-    <p>All rights reserved</p>
     <p>Contact information: <a href="">HelpSaveOurPlantWithTrees@gmail.com</a></p>
     </footer>
 </div>
